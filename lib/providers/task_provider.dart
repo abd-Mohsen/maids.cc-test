f6a2cc8cf9877dbfaece50c0762d7f22b61f19e6
Abd Moh <abdpuni6pusl9@gmail.com>
2024-06-10 00:43:09 +0300

    implemented pagination

--- lib/providers/task_provider.dart
+++ lib/providers/task_provider.dart
@@ -1,8 +1,6 @@
 import 'dart:async';
-
-import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 import 'package:maids.cc_test/constants.dart';
-
 import '../models/task_model.dart';
 import '../services/remote_services.dart';
 
@@ -33,20 +31,35 @@ class TaskProvider extends ChangeNotifier {
 
   //todo: refresh indicator + refresh task request
 
+  int page = 0, limit = 10;
+  bool hasMore = true;
+
+  Future<void> refreshTasks() async {
+    page = 0;
+    hasMore = true;
+    tasks.clear();
+    getTasks();
+  }
+
   Future<void> getTasks() async {
-    //todo: pagination
-    toggleLoading(true);
+    if (page == 0) toggleLoading(true);
     try {
-      List<TaskModel>? result = await RemoteServices.fetchTasks().timeout(kTimeOutDuration1);
+      List<TaskModel>? result = await RemoteServices.fetchTasks(
+        limit: limit,
+        skip: limit * page,
+      ).timeout(kTimeOutDuration1);
       if (result == null) {
         print("error getting tasks, check your connection"); //todo: show dialog
+      } else {
+        if (result.length < 10) hasMore = false;
+        tasks.addAll(result);
       }
-      tasks.addAll(result ?? []);
     } on TimeoutException {
       print("request timed out, check your connection"); //todo: show dialog
     } catch (e) {
       //
     }
     toggleLoading(false);
+    page++;
   }
 }
--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -23,7 +23,20 @@ class _HomePageState extends State<HomePage> {
       Provider.of<TaskProvider>(context, listen: false).getTasks();
       Provider.of<UserProvider>(context, listen: false).getAuthUser();
     });
+    scrollController.addListener(() {
+      if (scrollController.position.maxScrollExtent == scrollController.offset) {
+        Provider.of<TaskProvider>(context, listen: false).getTasks();
+      }
+    });
   }
+
+  @override
+  void dispose() {
+    scrollController.dispose();
+    super.dispose();
+  }
+
+  ScrollController scrollController = ScrollController();
 
   void logout() {
     prefs.remove("token");
@@ -37,93 +50,109 @@ class _HomePageState extends State<HomePage> {
     ColorScheme cs = Theme.of(context).colorScheme;
 
     return SafeArea(
-      child: Scaffold(
-        backgroundColor: cs.background,
-        appBar: AppBar(
-          title: Text(
-            "all tasks",
+      child: RefreshIndicator(
+        onRefresh: Provider.of<TaskProvider>(context, listen: false).refreshTasks,
+        child: Scaffold(
+          backgroundColor: cs.background,
+          appBar: AppBar(
+            title: const Text("all tasks"),
+            backgroundColor: cs.surface,
           ),
-          backgroundColor: cs.surface,
-        ),
-        drawer: Drawer(
-          child: Column(
-            children: [
-              Consumer<UserProvider>(
-                builder: (_, userProvider, child) => userProvider.isLoading
-                    ? Padding(
-                        padding: const EdgeInsets.all(16.0),
-                        child: Center(child: CircularProgressIndicator(color: cs.onBackground)),
-                      )
-                    : UserAccountsDrawerHeader(
-                        accountName: Text(userProvider.currentUser?.firstName ?? ""),
-                        accountEmail: Text(userProvider.currentUser?.email ?? ""),
-                        currentAccountPicture: CircleAvatar(
-                          child:
-                              userProvider.currentUser == null ? null : Image.network(userProvider.currentUser!.image),
+          drawer: Drawer(
+            child: Column(
+              children: [
+                Consumer<UserProvider>(
+                  builder: (_, userProvider, child) => userProvider.isLoading
+                      ? Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Center(child: CircularProgressIndicator(color: cs.onBackground)),
+                        )
+                      : UserAccountsDrawerHeader(
+                          accountName: Text(userProvider.currentUser?.firstName ?? ""),
+                          accountEmail: Text(userProvider.currentUser?.email ?? ""),
+                          currentAccountPicture: CircleAvatar(
+                            child: userProvider.currentUser == null
+                                ? null
+                                : Image.network(userProvider.currentUser!.image),
+                          ),
                         ),
-                      ),
-              ),
-              //todo: make light mode if there is time
-              ListTile(
-                leading: const Icon(
-                  Icons.info_outline,
                 ),
-                title: Text(
-                  "about app",
-                  style: tt.titleLarge,
-                ),
-                onTap: () => showDialog(
-                  context: context,
-                  builder: (context) => AlertDialog(
-                    title: const Text("Flutter developer test"),
-                    content: const Text("test project that demonstrates proficiency in Flutter development. The test "
-                        "project will focus on various aspects, including clean code & architecture, "
-                        "state management patterns, performance optimization, handling local storage, "
-                        "and writing unit tests\n\ntest is offered by: maids.cc company\n\n"
-                        "developed by: Abdulrahman Mohsen."),
-                    actions: [
-                      TextButton(
-                        onPressed: () => navigatorKey.currentState!.pop(),
-                        child: Text("ok"),
-                      )
-                    ],
+                //todo: make light mode if there is time
+                ListTile(
+                  leading: const Icon(
+                    Icons.info_outline,
+                  ),
+                  title: Text(
+                    "about app",
+                    style: tt.titleLarge,
+                  ),
+                  onTap: () => showDialog(
+                    context: context,
+                    builder: (context) => AlertDialog(
+                      title: const Text("Flutter developer test"),
+                      content: const Text("test project that demonstrates proficiency in Flutter development. The test "
+                          "project will focus on various aspects, including clean code & architecture, "
+                          "state management patterns, performance optimization, handling local storage, "
+                          "and writing unit tests\n\ntest is offered by: maids.cc company\n\n"
+                          "developed by: Abdulrahman Mohsen."),
+                      actions: [
+                        TextButton(
+                          onPressed: () => navigatorKey.currentState!.pop(),
+                          child: const Text("ok"),
+                        )
+                      ],
+                    ),
                   ),
                 ),
-              ),
-              ListTile(
-                leading: const Icon(
-                  Icons.logout,
-                  color: Colors.red,
+                ListTile(
+                  leading: const Icon(
+                    Icons.logout,
+                    color: Colors.red,
+                  ),
+                  title: Text(
+                    "logout",
+                    style: tt.titleLarge,
+                  ),
+                  onTap: () => logout(),
                 ),
-                title: Text(
-                  "logout",
-                  style: tt.titleLarge,
-                ),
-                onTap: () => logout(),
-              ),
-            ],
+              ],
+            ),
           ),
+          floatingActionButton: FloatingActionButton(
+            child: const Icon(Icons.add),
+            onPressed: () {
+              showModalBottomSheet(
+                context: context,
+                builder: (context) => const AddTaskBottomSheet(),
+              );
+            },
+          ),
+          body: Consumer<TaskProvider>(builder: (_, taskProvider, child) {
+            return taskProvider.isLoading
+                ? Center(child: CircularProgressIndicator(color: cs.onBackground))
+                : ListView.builder(
+                    controller: scrollController,
+                    itemCount: taskProvider.tasks.length + 1,
+                    itemBuilder: (context, i) {
+                      if (i < taskProvider.tasks.length) {
+                        TaskModel task = taskProvider.tasks[i];
+                        return TaskCard(task: task);
+                      }
+                      return Center(
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 24),
+                          child: taskProvider.hasMore
+                              ? CircularProgressIndicator(color: cs.primary)
+                              : CircleAvatar(
+                                  radius: 5,
+                                  backgroundColor: Colors.grey.withOpacity(0.7),
+                                ),
+                        ),
+                      );
+                    },
+                  );
+          }),
         ),
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add),
-          onPressed: () {
-            showModalBottomSheet(
-              context: context,
-              builder: (context) => AddTaskBottomSheet(),
-            );
-          },
-        ),
-        body: Consumer<TaskProvider>(builder: (_, taskProvider, child) {
-          return taskProvider.isLoading
-              ? Center(child: CircularProgressIndicator(color: cs.onBackground))
-              : ListView.builder(
-                  itemCount: taskProvider.tasks.length,
-                  itemBuilder: (context, i) {
-                    TaskModel task = taskProvider.tasks[i];
-                    return TaskCard(task: task);
-                  },
-                );
-        }),
       ),
     );
   }
